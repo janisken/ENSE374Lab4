9974cd7ffa560b56e9b3bc632e3461464787ae38
janisken <noah.janiskevich@gmail.com>
2015-12-01 09:17:40 -0600

    Update Grasshopper.java
    Added Coordinates

--- World/Grasshopper.java
+++ World/Grasshopper.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Grasshopper extends Animals{
 	
-	public Grasshopper(int canMove)
+	public Grasshopper(int canMove, int xCoor, int yCoor)
 	{
-		super("Grasshopper", canMove);
+		super("Grasshopper", canMove, xCoor, yCoor);
 	}
 }

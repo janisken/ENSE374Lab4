91e33ddee0b5c877beaf5f43767fffbf9d70e33c
janisken <noah.janiskevich@gmail.com>
2015-12-01 09:14:04 -0600

    Update Caterpillar.java
    Added Coordinates

--- World/Caterpillar.java
+++ World/Caterpillar.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Caterpillar extends Animals{
 	
-	public Caterpillar(int canMove)
+	public Caterpillar(int canMove, int xCoor, int yCoor)
 	{
-		super("Caterpillar", canMove);
+		super("Caterpillar", canMove, xCoor, yCoor);
 	}
 }

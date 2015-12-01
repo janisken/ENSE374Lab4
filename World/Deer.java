4725ff009196abc9a77c9c34e36b033dd712e84e
janisken <noah.janiskevich@gmail.com>
2015-12-01 09:14:31 -0600

    Update Deer.java
    Added Coordinates

--- World/Deer.java
+++ World/Deer.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Deer extends Animals{
 	
-	public Deer(int canMove)
+	public Deer(int canMove, int xCoor, int yCoor)
 	{
-		super("Deer", canMove);
+		super("Deer", canMove, int xCoor, int yCoor);
 	}
 }
